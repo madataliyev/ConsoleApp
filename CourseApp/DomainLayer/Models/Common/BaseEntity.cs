b09ca55f8c6d7cc9f436e92a222cf98756364066
madataliyev <madatma-apa202@code.edu.az>
2026-04-13 07:52:13 +0400

    1-DomainLayer

---
+++ CourseApp/DomainLayer/Models/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseApp.Domain.Models.Shared;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+}
---
+++ CourseApp/DomainLayer/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using CourseApp.Domain.Models.Shared;
+
+namespace CourseApp.Domain.Models;
+
+public class Student : BaseEntity
+{
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public CourseGroup CourseGroup { get; set; }
+    public static int _id;
+    public Student()
+    {
+        Id = _id;
+        _id++;
+    }
+
+}
